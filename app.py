70aa41a32da4dd090a3eb7ee7d0805daf23eb50a
darlithydd-ops <darlithydd@gmail.com>
2026-04-20 17:54:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+import streamlit as st
 
+st.set_page_config(page_title='Python Mentor', layout='wide')
+
+if 'logged_in' not in st.session_state:
+    st.session_state.logged_in = False
+  
+login_page = st.Page('pages/login.py', title='Вход в систему', icon='')
+trainer_page = st.Page('pages/trainer.py', title='Тренажер', icon='🐍')
+
+if st.session_state.logged_in:
+    pg = st.navigation([trainer_page])
+else:
+    pg = st.navigation([login_page])
+
+pg.run()
