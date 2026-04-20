cfc801256631236780c69a3ead8605f0bd19ea34
darlithydd-ops <darlithydd@gmail.com>
2026-04-20 18:12:15 +0800

    Implement basic Streamlit UI for trainer
    Add Streamlit interface for trainer page with greeting and exit button.

--- pages/trainer.py
+++ pages/trainer.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+st.title(f"Привет, {st.session_state.get('user_name', 'Студент')}!")
+st.write("Тренажер находится в разработке. Скоро здесь появятся задачи!")
+
+if st.button("Выйти"):
+    st.session_state.logged_in = False
+    st.rerun()
