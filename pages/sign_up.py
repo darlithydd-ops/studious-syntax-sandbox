e0a5b6f3d733f9ff6cbb43320f4f382281da504a
darlithydd-ops <darlithydd@gmail.com>
2026-04-22 18:10:02 +0800

    Update sign_up.py

--- pages/sign_up.py
+++ pages/sign_up.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
+conn = st.connection('postgresql', type='sql')
+
 left_col, center_col, right_col = st.columns([0.35, 0.3, 0.35])
 
 with center_col:
-  with st.container(border=True):
-    st.header('ok')
+    with st.container(border=True):
+        st.header('ok')
+      
   
