54850b39d8b9cb425ab79fe61feee026a2e10993
darlithydd-ops <darlithydd@gmail.com>
2026-04-22 17:34:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ st.set_page_config(page_title='Python Gym', layout='wide')
 
 if 'logged_in' not in st.session_state:
     st.session_state.logged_in = False
-  
+
+if 'sign_up_button_pushed' not in st.session_state:
+    st.session_state.sign_up_button_pushed = False
+     
 login_page = st.Page('pages/login.py', title='Вход в систему', icon='🐍')
 sign_up_page = st.Page('pages/sign_up.py', title='Вход в систему', icon='🐍')
 trainer_page = st.Page('pages/trainer.py', title='Тренажер', icon='🐍')
@@ -12,6 +15,9 @@ trainer_page = st.Page('pages/trainer.py', title='Тренажер', icon='🐍'
 if st.session_state.logged_in:
     pg = st.navigation([trainer_page])
 else:
-    pg = st.navigation([login_page])
+    if session_state.sign_up_button_pushed:
+        pg = st.navigation([sign_up_page])
+    else:
+        pg = st.navigation([login_page])
 
 pg.run()
