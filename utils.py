d14c5f3c9f01772bb74f0cb810b56a930e22522d
darlithydd-ops <darlithydd@gmail.com>
2026-04-23 01:29:01 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,24 @@
+import smtplib
+import streamlit as st
+from email.message import EmailMessage
 
+def send_email(receiver_email, content, subject):
+    
+    smtp_port = 465
+    smtp_server = st.secrets['emails']['smtp_server']
+    sender_email = st.secrets['emails']['user']
+    sender_password = st.secrets['emails']['password']
+
+    msg = EmailMessage()
+    msg.set_content(content)
+    msg['Subject'] = subject
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    try:
+        with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+            server.login(sender_email, sender_password)
+            server.send_message(msg)
+        return True
+    except Exception:
+        return False
