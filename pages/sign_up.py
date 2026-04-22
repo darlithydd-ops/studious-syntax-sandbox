0f2a477c322418495bb8d787dc950dfaa3296bbf
darlithydd-ops <darlithydd@gmail.com>
2026-04-22 19:40:22 +0800

    Update sign_up.py

--- pages/sign_up.py
+++ pages/sign_up.py
@@ -11,19 +11,15 @@ with center_col:
         
         selected_group = st.selectbox('Группа',
                                       options=df_groups['name'],
-                                      index=None,
-                                      placeholder='Группа',
-                                      label_visibility='collapsed')
-        
-        if selected_group:
+                                      index=1)
+    
+        selected_group_id = df_groups.loc[df_groups['name'] == selected_group, 'id'].values[0]
+        query = 'SELECT full_name FROM users WHERE group_id = :g_id ORDER BY full_name;'
+        df_students = conn.query(query, params={'g_id': int(selected_group_id)})
             
-            selected_group_id = df_groups.loc[df_groups['name'] == selected_group, 'id'].values[0]
-            query = 'SELECT full_name FROM users WHERE group_id = :g_id ORDER BY full_name;'
-            df_students = conn.query(query, params={'g_id': int(selected_group_id)})
-            
-            selected_full_name = st.selectbox('Полное имя',
-                                              options=df_students['full_name'],
-                                              index=None,
-                                              placeholder='Полное имя',
-                                              label_visibility='collapsed')
+        selected_full_name = st.selectbox('Полное имя',
+                                          options=df_students['full_name'],
+                                          index=None,
+                                          placeholder='Полное имя',
+                                          label_visibility='collapsed')
   
