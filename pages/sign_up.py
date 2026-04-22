c635845badfd1dd9d46ad128e2c614109a2a606f
darlithydd-ops <darlithydd@gmail.com>
2026-04-22 19:26:20 +0800

    Refactor student selection based on group
    Refactor student selection logic to check for selected group before querying.

--- pages/sign_up.py
+++ pages/sign_up.py
@@ -12,13 +12,13 @@ with center_col:
                                       index=None,
                                       placeholder='Группа',
                                       label_visibility='collapsed')
-        query = 'SELECT full_name FROM users WHERE group_id = :g_id ORDER BY full_name;'
-        selected_group_id = df_groups.loc[df_groups['name'] == selected_group, 'id'].values[0]
-        df_students = conn.query(query, params={'g_id': selected_group_id})
-        selected_full_name = st.selectbox('Полное имя',
-                                      options=df_students['full_name'],
-                                      index=None,
-                                      placeholder='Полное имя',
-                                      label_visibility='collapsed')
-      
+        if selected_group:
+            selected_group_id = df_groups.loc[df_groups['name'] == selected_group, 'id'].values[0]
+            query = 'SELECT full_name FROM users WHERE group_id = :g_id ORDER BY full_name;'
+            df_students = conn.query(query, params={'g_id': selected_group_id})
+            selected_full_name = st.selectbox('Полное имя',
+                                              options=df_students['full_name'],
+                                              index=None,
+                                              placeholder='Полное имя',
+                                              label_visibility='collapsed')
   
