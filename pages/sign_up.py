188ada332127404e2d9fc527d7107b9a40ca66cb
darlithydd-ops <darlithydd@gmail.com>
2026-04-22 18:36:08 +0800

    Update selectbox label and visibility in sign_up.py

--- pages/sign_up.py
+++ pages/sign_up.py
@@ -7,9 +7,11 @@ left_col, center_col, right_col = st.columns([0.35, 0.3, 0.35])
 with center_col:
     with st.container(border=True):
         df_groups = conn.query('SELECT name FROM groups ORDER BY name;')
-        selected_group = st.selectbox("Выберите группу для работы:",
-                                        options=df_groups['name'],
-                                        index=None,
-                                        placeholder="Выберите вариант или введите название...")
+        selected_group = st.selectbox('Группа',
+                                      options=df_groups['name'],
+                                      index=None,
+                                      placeholder='Группа',
+                                      label_visibility='collapsed')
+        
       
   
