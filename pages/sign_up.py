aef079cf60161407bf6899624ae15b8738707bc0
darlithydd-ops <darlithydd@gmail.com>
2026-04-22 18:29:02 +0800

    Refactor group selection code in sign_up.py

--- pages/sign_up.py
+++ pages/sign_up.py
@@ -6,11 +6,10 @@ left_col, center_col, right_col = st.columns([0.35, 0.3, 0.35])
 
 with center_col:
     with st.container(border=True):
-        with st.expander('Группа'):
-            df_groups = conn.query('SELECT name FROM groups ORDER BY name;')
-            selected_group = st.selectbox("Выберите группу для работы:",
-                                          options=df_groups['name'],
-                                          index=None,
-                                          placeholder="Выберите вариант или введите название...")
+        df_groups = conn.query('SELECT name FROM groups ORDER BY name;')
+        selected_group = st.selectbox("Выберите группу для работы:",
+                                        options=df_groups['name'],
+                                        index=None,
+                                        placeholder="Выберите вариант или введите название...")
       
   
