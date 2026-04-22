7d84e4fabb3bc5c693055dbf604b2a92f015713f
darlithydd-ops <darlithydd@gmail.com>
2026-04-22 19:32:00 +0800

    Implement group and student selection in sign up
    Add functionality to select group and corresponding students.

--- pages/sign_up.py
+++ pages/sign_up.py
@@ -6,16 +6,21 @@ left_col, center_col, right_col = st.columns([0.35, 0.3, 0.35])
 
 with center_col:
     with st.container(border=True):
+        
         df_groups = conn.query('SELECT id, name FROM groups ORDER BY name;')
+        
         selected_group = st.selectbox('Группа',
                                       options=df_groups['name'],
                                       index=None,
                                       placeholder='Группа',
                                       label_visibility='collapsed')
+        
         if selected_group:
+            
             selected_group_id = df_groups.loc[df_groups['name'] == selected_group, 'id'].values[0]
             query = 'SELECT full_name FROM users WHERE group_id = :g_id ORDER BY full_name;'
             df_students = conn.query(query, params={'g_id': int(selected_group_id)})
+            
             selected_full_name = st.selectbox('Полное имя',
                                               options=df_students['full_name'],
                                               index=None,
