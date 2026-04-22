62ee568be273a516b3cab71357474ce7214eda98
darlithydd-ops <darlithydd@gmail.com>
2026-04-22 13:44:59 +0800

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -2,31 +2,27 @@ import streamlit as st
 
 conn = st.connection('postgresql', type='sql')
 
-#st.title("Добро пожаловать в Тренажер")
+left_col, center_col, right_col = st.columns([0.3, 0.4, 0.3])
 
-tab_login, tab_reg = st.tabs(["Вход", "Регистрация"])
-
-with tab_login:
-    left_col, right_col = st.columns([0.3, 0.7])
-    with left_col:
-        with st.container(border=True):
-            st.markdown("<h2 style='text-align: center;'>Python Gym</h2>", unsafe_allow_html=True)
-            login_input = st.text_input("Логин")
-            pass_input = st.text_input("Пароль", type="password")
-            if st.button("Войти"):
-                user = conn.query(
-                    "SELECT id, full_name FROM users WHERE login = :l AND password_hash = :p",
-                    params={"l": login_input, "p": pass_input}
-                )
-                if not user.empty:
-                    st.session_state.logged_in = True
-                    st.session_state.user_name = user['full_name'].iloc[0]
-                    st.session_state.user_id = int(user['id'].iloc[0])
-                    st.success("Успешный вход!")
-                    st.rerun()
-                else:
-                    st.error("Неверный логин или пароль")
-
+with center_col:
+    with st.container(border=True):
+        st.markdown("<h2 style='text-align: center;'>Python Gym</h2>", unsafe_allow_html=True)
+        login_input = st.text_input('Логин')
+        pass_input = st.text_input('Пароль', type='password')
+        if st.button('Войти', width='stretch'):
+            user = conn.query(
+            "SELECT id, full_name FROM users WHERE login = :l AND password_hash = :p",
+            params={"l": login_input, "p": pass_input}
+            )
+            if not user.empty:
+                st.session_state.logged_in = True
+                st.session_state.user_name = user['full_name'].iloc[0]
+                st.session_state.user_id = int(user['id'].iloc[0])
+                st.success("Успешный вход!")
+                st.rerun()
+            else:
+                st.error("Неверный логин или пароль")
+"""
 with tab_reg:
     st.subheader("Создание аккаунта")
     
@@ -62,3 +58,4 @@ with tab_reg:
     else:
         st.info("В этой группе нет свободных мест.")
 
+"""
