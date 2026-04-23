c511d85a1931b3ba45eb339f1615320c01c5e595
darlithydd-ops <darlithydd@gmail.com>
2026-04-23 22:18:36 +0800

    Update sign_up.py

--- pages/sign_up.py
+++ pages/sign_up.py
@@ -16,9 +16,7 @@ with center_col:
         
         selected_group = st.selectbox('Группа',
                                       options=df_groups['name'],
-                                      index=None,
-                                      placeholder='Группа',
-                                      label_visibility='collapsed')
+                                      index=0)
         
         selected_group_id = df_groups.loc[df_groups['name'] == selected_group, 'id'].values[0]
         query = 'SELECT full_name FROM users WHERE group_id = :g_id ORDER BY full_name;'
