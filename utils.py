9031615e1f18ba4dee778849da2110a6fc31e984
darlithydd-ops <darlithydd@gmail.com>
2026-04-23 18:07:18 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -57,7 +57,7 @@ def st_funny(text, size=40, color="#FF4B4B", center=True):
         font-size: {size}px;
         color: {color};
         text-align: {align};
-        font-weight: 100;
+        font-weight: 700;
         margin: 10px 0px;
     }}
     </style>
