34da2deac3b4fcee5188326d78ca97eedfa86b95
darlithydd-ops <darlithydd@gmail.com>
2026-04-23 21:04:30 +0800

    Refactor sign-up page styling and clean up code
    Removed commented-out code and adjusted styling function calls for 'Python Gym'.

--- pages/sign_up.py
+++ pages/sign_up.py
@@ -9,12 +9,7 @@ left_col, center_col, right_col = st.columns([0.35, 0.3, 0.35])
 
 with center_col:
     with st.container(border=True):
-
-        #st.markdown("<h3 style='text-align: center;'>Python Gym</h3>", unsafe_allow_html=True)
-        
-        apply_comic_style('Python Gym')
         apply_caveat_style('Python Gym', color='#E6E6FA')
-        apply_pixel_style('Python Gym')
         df_groups = conn.query('SELECT id, name FROM groups ORDER BY name;')
         
         selected_group = st.selectbox('Группа',
