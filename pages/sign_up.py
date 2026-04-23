570233d28ec67c481e994bf6a5f7094d6658fae6
darlithydd-ops <darlithydd@gmail.com>
2026-04-23 21:16:45 +0800

    Update sign_up.py

--- pages/sign_up.py
+++ pages/sign_up.py
@@ -9,12 +9,16 @@ left_col, center_col, right_col = st.columns([0.35, 0.3, 0.35])
 
 with center_col:
     with st.container(border=True):
+        
         apply_caveat_style('Python Gym', color='#D8BFD8')
+        
         df_groups = conn.query('SELECT id, name FROM groups ORDER BY name;')
         
         selected_group = st.selectbox('Группа',
                                       options=df_groups['name'],
-                                      index=1)
+                                      index=0,
+                                      placeholder='Группа',
+                                      label_visibility='collapsed')
         
         selected_group_id = df_groups.loc[df_groups['name'] == selected_group, 'id'].values[0]
         query = 'SELECT full_name FROM users WHERE group_id = :g_id ORDER BY full_name;'
